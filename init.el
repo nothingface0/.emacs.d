dd9cea106417d040d0f15ca0df143995328072a2
dpapagiannis <dpapagiannis@raymetrics.com>
2020-07-24 16:09:06 +0300

    Added clang-format+

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
 		  (funcall (cdr my-pair))
 		(message "Could not match regex \"%s\" to buffer file name, not enabling %s" (car my-pair) (cdr my-pair)))))
 
-(setq package-list '(use-package sr-speedbar markdown-mode tabbar arduino-mode flycheck magit autopair nocomments-mode highlight-doxygen auto-complete auctex jedi web-mode rainbow-mode shell-here js2-mode powershell transient highlight-indent-guides dakrone-theme json-mode yaml-mode auto-complete-auctex ac-math prettier-js python-black))
+(setq package-list '(use-package sr-speedbar markdown-mode tabbar arduino-mode flycheck magit autopair nocomments-mode highlight-doxygen auto-complete auctex jedi web-mode rainbow-mode shell-here js2-mode powershell transient highlight-indent-guides dakrone-theme json-mode yaml-mode auto-complete-auctex ac-math prettier-js python-black clang-format+))
 (setq package-archives '(("elpa" . "https://tromey.com/elpa/")
                          ("gnu" . "https://elpa.gnu.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")))
@@ -50,6 +50,9 @@ There are two things you can do about this warning:
 ;; Autocomplete configuration
 (ac-config-default)
 (global-auto-complete-mode 1)
+
+;; clang format for C/C++
+(add-hook 'c-mode-common-hook #'clang-format+-mode)
 
 ;; Enable server mode
 (server-start)
@@ -109,7 +112,7 @@ There are two things you can do about this warning:
 	("2593436c53c59d650c8e3b5337a45f0e1542b1ba46ce8956861316e860b145a0" "28caf31770f88ffaac6363acfda5627019cac57ea252ceb2d41d98df6d87e240" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" default)))
  '(package-selected-packages
    (quote
-	(prettier-js python-black exec-path-from-shell json-mode yaml-mode dakrone-theme dracula-theme python-pep8 js2-mode powershell jedi sr-speedbar tabbar arduino-mode flycheck magit autopair nocomments-mode highlight-doxygen auto-complete auctex markdown-mode shell-here rainbow-mode web-mode transient highlight-indent-guides use-package)))
+	(clang-format+ prettier-js python-black exec-path-from-shell json-mode yaml-mode dakrone-theme dracula-theme python-pep8 js2-mode powershell jedi sr-speedbar tabbar arduino-mode flycheck magit autopair nocomments-mode highlight-doxygen auto-complete auctex markdown-mode shell-here rainbow-mode web-mode transient highlight-indent-guides use-package)))
  '(tab-width 4))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -199,6 +202,9 @@ There are two things you can do about this warning:
 ;; LaTeX
 (setq-default TeX-engine 'xetex)
 (setq-default TeX-PDF-mode t)
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+
 
 ;; Save session on exit
 (desktop-save-mode 1)
