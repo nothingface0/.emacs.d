184e8a3e5b4f9e28750cca6f9688a26b13d39d05
Dimitris Papagiannis <dpapagiannis@raymetrics.com>
2021-06-28 19:21:26 +0300

    Added asm config (do not remember when I used it??)

--- init.el
+++ init.el
@@ -394,5 +394,14 @@ There are two things you can do about this warning:
                       (unless (eq (car args) 'flymake-mode)
                         (apply fun args))))
 
+(require 'asm-mode)
+(add-hook 'asm-mode-hook (lambda ()
+                           (setq indent-tabs-mode nil) ; use spaces to indent
+                           (electric-indent-mode -1) ; indentation in asm-mode is annoying
+                           (setq tab-stop-list (number-sequence 2 60 2))))
+
+(define-key asm-mode-map (kbd "<ret>") 'newline-and-indent)
+(define-key asm-mode-map (kbd "M-.") 'helm-etags-select)
+
 (provide '.emacs)
 ;;; init.el ends here
